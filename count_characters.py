5a6d83fd2896a6e8effe1a62d4ef28f014d0a67e
techstyle99 <techstyle99@gmail.com>
2021-03-27 14:31:26 -0400

    removing both textfiles

--- count_characters.py
+++
@@ -1,9 +0,0 @@
-
-
-
-
-
-file = open("/users/georgeaugust/desktop/webdev/visual_studio_code/test_project/project1/textfile.txt", "r")
-data = file.read().replace(" ", "")
-characters = len(data)
-print("There are " + str(characters) + " in this text file")
